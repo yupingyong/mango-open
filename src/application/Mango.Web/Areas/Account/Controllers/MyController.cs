8c6043d99833553bbb5d7f52901062acd4b17c0f
yupingyong <yupingyong@qq.com>
2019-12-04 23:15:18 +0800

    20191204-1

--- src/application/Mango.Web/Areas/Account/Controllers/MyController.cs
+++ src/application/Mango.Web/Areas/Account/Controllers/MyController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Newtonsoft.Json;
+using Mango.Core;
+using Mango.Web.Common;
+using Microsoft.AspNetCore.Http;
 namespace Mango.Web.Areas.Account.Controllers
 {
     [Area("Account")]
@@ -13,13 +16,31 @@ namespace Mango.Web.Areas.Account.Controllers
         {
             return View();
         }
-        public IActionResult Article()
+        [HttpGet]
+        public IActionResult Article([FromRoute]int p=1)
         {
-            return View();
+            int accountId = HttpContext.Session.GetInt32("AccountId").GetValueOrDefault(0);
+            Models.MyArticleViewModel viewModel = new Models.MyArticleViewModel();
+            var apiResult = HttpCore.HttpGet($"/api/CMS/Contents/user/list/{accountId}/{p}");
+
+            if (apiResult.Code == 0)
+            {
+                viewModel.ListData = JsonConvert.DeserializeObject<List<Models.ContentsListDataModel>>(apiResult.Data.ToString());
+            }
+            return View(viewModel);
         }
-        public IActionResult Theme()
+        [HttpGet]
+        public IActionResult Theme([FromRoute]int p = 1)
         {
-            return View();
+            int accountId = HttpContext.Session.GetInt32("AccountId").GetValueOrDefault(0);
+            Models.MyThemeViewModel viewModel = new Models.MyThemeViewModel();
+            var apiResult = HttpCore.HttpGet($"/api/Docs/Theme/user/{accountId}/{p}");
+
+            if (apiResult.Code == 0)
+            {
+                viewModel.ListData = JsonConvert.DeserializeObject<List<Models.ThemeDataModel>>(apiResult.Data.ToString());
+            }
+            return View(viewModel);
         }
         public IActionResult Document()
         {
